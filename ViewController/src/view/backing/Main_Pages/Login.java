ac3581ae4eb6516b6d5a13fbaf99f63a831a4016
MuhammadAhmad-OracleDeveloper <ahmad@ircoca.com>
2019-05-13 14:11:46 +0500

    database changed to 102 and dashboard working

--- ViewController/src/view/backing/Main_Pages/Login.java
+++ ViewController/src/view/backing/Main_Pages/Login.java
@@ -1,5 +1,7 @@
 package view.backing.Main_Pages;
 
+import java.io.Serializable;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,24 +11,22 @@ import java.sql.Statement;
 
 import java.util.Map;
 
-import javax.faces.component.html.HtmlGraphicImage;
+
+
+import javax.faces.component.UIComponent;
+
 
 import javax.faces.context.FacesContext;
-import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ActionEvent;
 
-import oracle.adf.view.rich.component.rich.RichDocument;
-import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.controller.v2.lifecycle.PagePhaseEvent;
 import oracle.adf.view.rich.component.rich.input.RichInputText;
-import oracle.adf.view.rich.component.rich.layout.RichGridCell;
-import oracle.adf.view.rich.component.rich.layout.RichGridRow;
-import oracle.adf.view.rich.component.rich.layout.RichPanelAccordion;
-import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
-import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
-import oracle.adf.view.rich.component.rich.layout.RichShowDetailItem;
-import oracle.adf.view.rich.component.rich.nav.RichLink;
-import oracle.adf.view.rich.component.rich.output.RichImage;
-import oracle.adf.view.rich.component.rich.output.RichSpacer;
 
+import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
+
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+import oracle.adf.view.rich.component.rich.nav.RichLink;
+import oracle.adf.view.rich.context.AdfFacesContext;
 import oracle.jdbc.driver.OracleDriver;
 
 public class Login {
@@ -48,393 +48,15 @@ public class Login {
     private static String save_rights;
     private static String sessUName;
 
-    private RichForm f1;
-    private RichDocument d1;
-    private RichPanelAccordion pa1;
-    private RichShowDetailItem pane1;
-    private RichPanelGridLayout pgl1;
-    private RichGridRow gr1;
-    private RichGridCell gc1;
-    private RichGridCell gc2;
-    private RichGridCell gc3;
-    private RichGridRow gr2;
-    private RichGridCell gc4;
-    private RichGridCell gc5;
-    private RichGridCell gc6;
-    private RichGridRow gr3;
-    private RichGridCell gc7;
-    private RichGridCell gc8;
-    private RichGridCell gc9;
-    private RichGridRow gr4;
-    private RichGridCell gc10;
-    private RichGridCell gc11;
-    private RichGridCell gc12;
-    private RichGridRow gr5;
-    private RichGridCell gc13;
-    private RichGridCell gc14;
-    private RichGridCell gc15;
-    private RichGridRow gr6;
-    private RichGridCell gc16;
-    private RichGridCell gc17;
-    private RichGridCell gc18;
-    private RichGridRow gr7;
-    private RichGridCell gc19;
-    private RichGridCell gc20;
-    private RichGridCell gc21;
-    private RichGridRow gr8;
-    private RichGridCell gc22;
-    private RichGridCell gc23;
-    private RichGridCell gc24;
-    private HtmlGraphicImage gi1;
-    private RichImage i1;
-    private RichPanelGroupLayout pgl2;
-    private RichSpacer s1;
-    private RichImage i2;
+
+    /////////////////////////////////////////
+
     private RichInputText it1;
-    private RichSpacer s2;
-    private RichPanelGroupLayout pgl3;
-    private RichSpacer s3;
-    private RichImage i3;
     private RichInputText it2;
-    private RichSpacer s4;
     private RichLink l1;
-    private RichSpacer s5;
+    private RichPanelFormLayout panel_form_layout;
+    private RichButton gen;
 
-    public void setF1(RichForm f1) {
-        this.f1 = f1;
-    }
-
-    public RichForm getF1() {
-        return f1;
-    }
-
-    public void setD1(RichDocument d1) {
-        this.d1 = d1;
-    }
-
-    public RichDocument getD1() {
-        return d1;
-    }
-
-    public void setPa1(RichPanelAccordion pa1) {
-        this.pa1 = pa1;
-    }
-
-    public RichPanelAccordion getPa1() {
-        return pa1;
-    }
-
-    public void setPane1(RichShowDetailItem pane1) {
-        this.pane1 = pane1;
-    }
-
-    public RichShowDetailItem getPane1() {
-        return pane1;
-    }
-
-    public void setPgl1(RichPanelGridLayout pgl1) {
-        this.pgl1 = pgl1;
-    }
-
-    public RichPanelGridLayout getPgl1() {
-        return pgl1;
-    }
-
-    public void setGr1(RichGridRow gr1) {
-        this.gr1 = gr1;
-    }
-
-    public RichGridRow getGr1() {
-        return gr1;
-    }
-
-    public void setGc1(RichGridCell gc1) {
-        this.gc1 = gc1;
-    }
-
-    public RichGridCell getGc1() {
-        return gc1;
-    }
-
-    public void setGc2(RichGridCell gc2) {
-        this.gc2 = gc2;
-    }
-
-    public RichGridCell getGc2() {
-        return gc2;
-    }
-
-    public void setGc3(RichGridCell gc3) {
-        this.gc3 = gc3;
-    }
-
-    public RichGridCell getGc3() {
-        return gc3;
-    }
-
-    public void setGr2(RichGridRow gr2) {
-        this.gr2 = gr2;
-    }
-
-    public RichGridRow getGr2() {
-        return gr2;
-    }
-
-    public void setGc4(RichGridCell gc4) {
-        this.gc4 = gc4;
-    }
-
-    public RichGridCell getGc4() {
-        return gc4;
-    }
-
-    public void setGc5(RichGridCell gc5) {
-        this.gc5 = gc5;
-    }
-
-    public RichGridCell getGc5() {
-        return gc5;
-    }
-
-    public void setGc6(RichGridCell gc6) {
-        this.gc6 = gc6;
-    }
-
-    public RichGridCell getGc6() {
-        return gc6;
-    }
-
-    public void setGr3(RichGridRow gr3) {
-        this.gr3 = gr3;
-    }
-
-    public RichGridRow getGr3() {
-        return gr3;
-    }
-
-    public void setGc7(RichGridCell gc7) {
-        this.gc7 = gc7;
-    }
-
-    public RichGridCell getGc7() {
-        return gc7;
-    }
-
-    public void setGc8(RichGridCell gc8) {
-        this.gc8 = gc8;
-    }
-
-    public RichGridCell getGc8() {
-        return gc8;
-    }
-
-    public void setGc9(RichGridCell gc9) {
-        this.gc9 = gc9;
-    }
-
-    public RichGridCell getGc9() {
-        return gc9;
-    }
-
-    public void setGr4(RichGridRow gr4) {
-        this.gr4 = gr4;
-    }
-
-    public RichGridRow getGr4() {
-        return gr4;
-    }
-
-    public void setGc10(RichGridCell gc10) {
-        this.gc10 = gc10;
-    }
-
-    public RichGridCell getGc10() {
-        return gc10;
-    }
-
-    public void setGc11(RichGridCell gc11) {
-        this.gc11 = gc11;
-    }
-
-    public RichGridCell getGc11() {
-        return gc11;
-    }
-
-    public void setGc12(RichGridCell gc12) {
-        this.gc12 = gc12;
-    }
-
-    public RichGridCell getGc12() {
-        return gc12;
-    }
-
-    public void setGr5(RichGridRow gr5) {
-        this.gr5 = gr5;
-    }
-
-    public RichGridRow getGr5() {
-        return gr5;
-    }
-
-    public void setGc13(RichGridCell gc13) {
-        this.gc13 = gc13;
-    }
-
-    public RichGridCell getGc13() {
-        return gc13;
-    }
-
-    public void setGc14(RichGridCell gc14) {
-        this.gc14 = gc14;
-    }
-
-    public RichGridCell getGc14() {
-        return gc14;
-    }
-
-    public void setGc15(RichGridCell gc15) {
-        this.gc15 = gc15;
-    }
-
-    public RichGridCell getGc15() {
-        return gc15;
-    }
-
-    public void setGr6(RichGridRow gr6) {
-        this.gr6 = gr6;
-    }
-
-    public RichGridRow getGr6() {
-        return gr6;
-    }
-
-    public void setGc16(RichGridCell gc16) {
-        this.gc16 = gc16;
-    }
-
-    public RichGridCell getGc16() {
-        return gc16;
-    }
-
-    public void setGc17(RichGridCell gc17) {
-        this.gc17 = gc17;
-    }
-
-    public RichGridCell getGc17() {
-        return gc17;
-    }
-
-    public void setGc18(RichGridCell gc18) {
-        this.gc18 = gc18;
-    }
-
-    public RichGridCell getGc18() {
-        return gc18;
-    }
-
-    public void setGr7(RichGridRow gr7) {
-        this.gr7 = gr7;
-    }
-
-    public RichGridRow getGr7() {
-        return gr7;
-    }
-
-    public void setGc19(RichGridCell gc19) {
-        this.gc19 = gc19;
-    }
-
-    public RichGridCell getGc19() {
-        return gc19;
-    }
-
-    public void setGc20(RichGridCell gc20) {
-        this.gc20 = gc20;
-    }
-
-    public RichGridCell getGc20() {
-        return gc20;
-    }
-
-    public void setGc21(RichGridCell gc21) {
-        this.gc21 = gc21;
-    }
-
-    public RichGridCell getGc21() {
-        return gc21;
-    }
-
-    public void setGr8(RichGridRow gr8) {
-        this.gr8 = gr8;
-    }
-
-    public RichGridRow getGr8() {
-        return gr8;
-    }
-
-    public void setGc22(RichGridCell gc22) {
-        this.gc22 = gc22;
-    }
-
-    public RichGridCell getGc22() {
-        return gc22;
-    }
-
-    public void setGc23(RichGridCell gc23) {
-        this.gc23 = gc23;
-    }
-
-    public RichGridCell getGc23() {
-        return gc23;
-    }
-
-    public void setGc24(RichGridCell gc24) {
-        this.gc24 = gc24;
-    }
-
-    public RichGridCell getGc24() {
-        return gc24;
-    }
-
-    public void setGi1(HtmlGraphicImage gi1) {
-        this.gi1 = gi1;
-    }
-
-    public HtmlGraphicImage getGi1() {
-        return gi1;
-    }
-
-    public void setI1(RichImage i1) {
-        this.i1 = i1;
-    }
-
-    public RichImage getI1() {
-        return i1;
-    }
-
-    public void setPgl2(RichPanelGroupLayout pgl2) {
-        this.pgl2 = pgl2;
-    }
-
-    public RichPanelGroupLayout getPgl2() {
-        return pgl2;
-    }
-
-    public void setS1(RichSpacer s1) {
-        this.s1 = s1;
-    }
-
-    public RichSpacer getS1() {
-        return s1;
-    }
-
-    public void setI2(RichImage i2) {
-        this.i2 = i2;
-    }
-
-    public RichImage getI2() {
-        return i2;
-    }
 
     public void setIt1(RichInputText it1) {
         this.it1 = it1;
@@ -443,38 +65,7 @@ public class Login {
     public RichInputText getIt1() {
         return it1;
     }
-
-    public void setS2(RichSpacer s2) {
-        this.s2 = s2;
-    }
-
-    public RichSpacer getS2() {
-        return s2;
-    }
-
-    public void setPgl3(RichPanelGroupLayout pgl3) {
-        this.pgl3 = pgl3;
-    }
-
-    public RichPanelGroupLayout getPgl3() {
-        return pgl3;
-    }
-
-    public void setS3(RichSpacer s3) {
-        this.s3 = s3;
-    }
-
-    public RichSpacer getS3() {
-        return s3;
-    }
-
-    public void setI3(RichImage i3) {
-        this.i3 = i3;
-    }
-
-    public RichImage getI3() {
-        return i3;
-    }
+     
 
     public void setIt2(RichInputText it2) {
         this.it2 = it2;
@@ -483,14 +74,7 @@ public class Login {
     public RichInputText getIt2() {
         return it2;
     }
-
-    public void setS4(RichSpacer s4) {
-        this.s4 = s4;
-    }
-
-    public RichSpacer getS4() {
-        return s4;
-    }
+    
 
     public void setL1(RichLink l1) {
         this.l1 = l1;
@@ -500,13 +84,7 @@ public class Login {
         return l1;
     }
 
-    public void setS5(RichSpacer s5) {
-        this.s5 = s5;
-    }
-
-    public RichSpacer getS5() {
-        return s5;
-    }
+    ///////////////////////////////////////////
 
 
     // creating generic database connection
@@ -569,7 +147,8 @@ public class Login {
                 //Redirecting to home page after successfull authentication
                 //System.out.println("....... here we go /// you are redirecting now to DASHBOARD ......");
                 //                return "good";
-                get_module_view_rights();
+//                get_module_view_rights();
+                
                 conn.close();
                 return "/faces/Main_Pages/Dashboard.jsf?faces-redirect=true";
             } else {
@@ -1087,5 +666,43 @@ public class Login {
         return "good";
     }
 
+
+//////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+
+    public void setPanel_form_layout(RichPanelFormLayout panel_form_layout) {
+        this.panel_form_layout = panel_form_layout;
+    }
+
+    public RichPanelFormLayout getPanel_form_layout() {
+        return panel_form_layout;
+    }
+    
+    public void addComponent(UIComponent parentUIComponent, UIComponent childUIComponent) {
+            parentUIComponent.getChildren().add(childUIComponent);
+            AdfFacesContext.getCurrentInstance().addPartialTarget(parentUIComponent);
+        }
+    
+    public void createComptext(ActionEvent actionEvent) {
+        RichInputText ui = new RichInputText();
+        ui.setId("rit1");
+        ui.setLabel("Input text");
+        ui.setValue("Hello ADF");
+        ui.setContentStyle("font-weight:bold;color:red");
+        addComponent(getPanel_form_layout(), ui);
+        
+//        ActionEvent actionEvent;
+//        RichOutputText uci = new RichOutputText();
+//        ui.setId("rot1");
+//        ui.setValue("I am output text");
+//        ui.setInlineStyle("font-weight:bold;color:green");
+//        addComponent(getPanel_form_layout(), uci);
+        System.out.println("On load DashBoard Design Called");
+    }
+    
+    public void afterPhase(PagePhaseEvent pagePhaseEvent) {
+        System.out.println("New.....");
+    }
 
 }
